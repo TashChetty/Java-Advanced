ca0321317795d9be46a13b2c24fee24fe0e7e0bd
TashChetty <natasha.chetty2@gmail.com>
2024-02-13 20:02:43 +0200

    igbook2

---
+++ igbook2/lesson13/SerializeStockExample/StockException.java
@@ -0,0 +1,8 @@
+package lesson13.SerializeStockExample;
+
+public class StockException extends Exception {
+    private static final long serialVersionUID = 103L;
+    public StockException (String reason) {
+        super (reason);
+    }
+}
