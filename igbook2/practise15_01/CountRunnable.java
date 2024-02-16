ac4b05befbe17546ed52ef9de2a526b745e265b3
TashChetty <natasha.chetty2@gmail.com>
2024-02-16 02:19:55 +0200

    igbook2

---
+++ igbook2/practise15_01/CountRunnable.java
@@ -0,0 +1,22 @@
+package practise15_01;
+
+public class CountRunnable implements Runnable{
+
+  final int count;
+  final String threadName;
+
+
+  public CountRunnable(int count, String name){
+    this.count = count;
+    this.threadName = name;
+  }
+
+  public void run(){
+
+    for (int i = 1; i <= count; i++){
+      System.out.println("Thread " + threadName +
+        ": " + i);
+    }
+  }
+}
+
