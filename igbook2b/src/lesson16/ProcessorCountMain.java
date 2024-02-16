b1dd36ecc77c70536a644f24a65f12e31cf33844
TashChetty <natasha.chetty2@gmail.com>
2024-02-16 14:32:02 +0200

    igbook2

---
+++ igbook2b/src/lesson16/ProcessorCountMain.java
@@ -0,0 +1,8 @@
+package lesson16;
+
+public class ProcessorCountMain {
+    public static void main(String[] args) {
+        int count = Runtime.getRuntime().availableProcessors();
+        System.out.println("Number of logical  processors' on this device is: " + count);
+    }
+}
